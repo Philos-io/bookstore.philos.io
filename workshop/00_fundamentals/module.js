8d67540fb034b6c0b43724a9150bf8f03e933e82
davy engone <davy@philos.io>
2015-11-24 20:37:19 +0100

    added module design pattern example

---
+++ workshop/00_fundamentals/module.js
@@ -0,0 +1,23 @@
+var bookService = function(){
+
+	function createBook(){}
+	function getBook(){}
+	function saveBook(){}
+	function updateBook(){}
+	function deleteBook(){}
+
+	// Public API
+	return {
+		get: getBook,
+		save: saveBook,
+		update: updateBook,
+		remove: deleteBook
+	}
+};
+
+// How do you consume that?
+var book = bookService();
+book.get();
+
+var book:Service = function(){
+}
