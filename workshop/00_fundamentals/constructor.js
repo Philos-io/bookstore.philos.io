02def9f96c7d9fd4662226370d2331396caaf061
davy engone <davy@philos.io>
2015-11-24 20:36:31 +0100

    added constructor design pattern

---
+++ workshop/00_fundamentals/constructor.js
@@ -0,0 +1,10 @@
+// Book model: Constructor
+var Book = function(options){
+	this.author = options.author;
+	this.topic = options.topic;
+};
+
+var book = new Book({author: 'Mehdi Mitchel', topic: 'Design for developers'});
+var book2 = new Book({author: 'Max Mitchel', topic: 'Product management done well'});
+
+console.info(book === book2);
