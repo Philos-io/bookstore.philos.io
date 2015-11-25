b028f76ee3a9e294b0ede0ddc233672682a8fd00
davy engone <davy@philos.io>
2015-11-25 07:10:20 +0100

    angularjs core concepts: binding, scope, controller, directive

---
+++ workshop/02_binding_scope_controller_directive/books.js
@@ -0,0 +1,18 @@
+var books = [{
+  bookID: 9809,
+  author: 'Davy Mitchel',
+  title: 'Functional JavaScript',
+  description: 'Functional programming in JavaScript',
+  price: 29.99,
+  category: 'Programming language',
+  cover: 'assets/img/javascript.jpg'
+},
+{
+  bookID: 9100,
+  author: 'Max Mitchel',
+  title: 'AngularJS: Up and Running',
+  description: 'Enhanced Productivity with Structured Web Apps',
+  price: 19.99,
+  category: 'Web Frameworks',
+  cover: 'assets/img/angularjs.jpg'
+}];
---
+++ workshop/02_binding_scope_controller_directive/bookstore.js
@@ -0,0 +1,13 @@
+angular.module('bookstore', []);
+
+//
+// 1. Add a BookController that contains a list of books
+// Then bind the corresponding view inside index.html
+//
+
+//
+// 2. Add a NavController that has a property title
+// Then bind it to the corresponding view inside index.html
+//
+
+angular.bootstrap(document.body, ['bookstore']);
